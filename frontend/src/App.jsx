46f60c82b98c434bdf875036208dd35a009b2c6b
@rcmnp275201 <rcmnp275201@gmail.com>
2026-06-17 14:27:07 +0530

    InitialBuild

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <> 
+    <div>Artificial Intelligence Blogs</div>
+    <ul>
+      <li>Blog 1: Introduction to Machine Learning</li>
+      <li>Blog 2: Deep Learning Techniques</li>
+      <li>Blog 3: Natural Language Processing</li>
+      <li>Blog 4: Computer Vision Applications</li>
+      <li>Blog 5: AI Ethics and Future Trends</li>
+    </ul>
+    </>
+  )
+}
+
+
+export default App
